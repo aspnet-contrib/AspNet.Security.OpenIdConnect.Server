1b09b604b5a4fd27db82f0b05d12eb8500e16d7b
Pinpoint <kevinchalet@gmail.com>
2014-07-11 23:00:02 +0200

    Remove ASP.NET Identity from the client sample

--- SampleOpenIdConnectClient/Startup.cs
+++ SampleOpenIdConnectClient/Startup.cs
@@ -1,40 +1,39 @@
-using System;
-using System.Collections.Concurrent;
-using System.IO;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
-using System.Threading.Tasks;
 using Microsoft.Owin;
-using Microsoft.Owin.Extensions;
-using Microsoft.Owin.Logging;
-using Microsoft.Owin.Security;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Owin;
-using System.IdentityModel.Tokens;
-using Microsoft.Owin.Security.OpenIdConnect;
-using Microsoft.IdentityModel.Protocols;
 using SampleOpenIdConnectClient;
-using Microsoft.Owin.Security.Cookies;
 
 [assembly: OwinStartup(typeof(Startup))]
 
 namespace SampleOpenIdConnectClient {
+    using System;
+    using System.IdentityModel.Tokens;
+    using System.Text;
+    using Microsoft.IdentityModel.Protocols;
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Security.Cookies;
+    using Microsoft.Owin.Security.OpenIdConnect;
+    using Owin;
+
     public class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureOidcClientDemo(app);
         }
 
         private static void ConfigureOidcClientDemo(IAppBuilder app) {
-            app.UseExternalSignInCookie("ExternalCookie");
+            app.SetDefaultSignInAsAuthenticationType("ExternalCookie");
 
-            var key = new InMemorySymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("secret_secret_secret"));
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationMode = AuthenticationMode.Passive,
+                AuthenticationType = "ExternalCookie",
+                CookieName = CookieAuthenticationDefaults.CookiePrefix + "ExternalCookie",
+                ExpireTimeSpan = TimeSpan.FromMinutes(5)
+            });
+
+            var key = new InMemorySymmetricSecurityKey(Encoding.UTF8.GetBytes("secret_secret_secret"));
 
             app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions {
                 AuthenticationMode = AuthenticationMode.Active,
                 AuthenticationType = "OIDC",
-                SignInAsAuthenticationType = "ExternalCookie",
+                SignInAsAuthenticationType = app.GetDefaultSignInAsAuthenticationType(),
                 ClientId = "myClient",
                 ClientSecret = "secret_secret_secret",
                 RedirectUri = "http://localhost:57264/oidc",
@@ -49,8 +48,6 @@ namespace SampleOpenIdConnectClient {
                     IssuerSigningKey = key
                 }
             });
-
-
         }
     }
 }
