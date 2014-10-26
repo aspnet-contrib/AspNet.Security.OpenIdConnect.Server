7e3592fda40d9743675c43d21a59c9278334dc2a
Kévin Chalet <kevinchalet@gmail.com>
2014-10-26 23:30:38 +0100

    Remove KeyAttribute from Nonce

--- samples/Mvc/Mvc.Server/Models/Nonce.cs
+++ samples/Mvc/Mvc.Server/Models/Nonce.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mvc.Server.Models {
+﻿namespace Mvc.Server.Models {
     public class Nonce {
-        [Key]
         public string NonceID { get; set; }
         public string Ticket { get; set; }
     }
