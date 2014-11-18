80eaa56dba32220b7d6d079bb9cf903cba463c8b
Pinpoint <kevinchalet@gmail.com>
2014-11-18 22:12:43 +0100

    Add a new ResourceController in Mvc.Server and update Mvc.Client to retrieve an access token and query the new API endpoint

--- samples/Mvc/Mvc.Client/Controllers/HomeController.cs
+++ samples/Mvc/Mvc.Client/Controllers/HomeController.cs
@@ -1,19 +1,45 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Microsoft.IdentityModel.Protocols;
 
 namespace Mvc.Client.Controllers {
     public class HomeController : Controller {
         [HttpGet, Route("~/")]
         public ActionResult Index() {
-            var principal = User as ClaimsPrincipal;
+            return View("Home");
+        }
 
-            // Determine whether the user agent has been successfully authenticated
-            // by the cookies middleware (configured with AuthenticationMode.Active in Startup.cs)
-            if (principal == null || principal.Identity == null || !principal.Identity.IsAuthenticated) {
-                principal = null;
+        [Authorize, HttpPost, Route("~/")]
+        public async Task<ActionResult> Index(CancellationToken cancellationToken) {
+            using (var client = new HttpClient()) {
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:55985/api/message");
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", AccessToken);
+
+                var response = await client.SendAsync(request, cancellationToken);
+                response.EnsureSuccessStatusCode();
+
+                return View("Home", model: await response.Content.ReadAsStringAsync());
             }
+        }
 
-            return View("Home", principal);
+        protected ClaimsPrincipal Principal {
+            get { return User as ClaimsPrincipal ?? new ClaimsPrincipal(User); }
+        }
+
+        protected string AccessToken {
+            get {
+                var claim = Principal.FindFirst(OpenIdConnectParameterNames.AccessToken);
+                if (claim == null) {
+                    throw new InvalidOperationException();
+                }
+
+                return claim.Value;
+            }
         }
     }
 }
