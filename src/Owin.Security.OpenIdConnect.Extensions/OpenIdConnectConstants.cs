5a35697f160cfb58ae97d668032cc1271e932371
Kévin Chalet <kevinchalet@gmail.com>
2015-04-02 20:00:50 +0200

    Merge Owin.Security.OpenIdConnect.Extensions and Owin.Security.OpenIdConnect.Server

---
+++ src/Owin.Security.OpenIdConnect.Extensions/OpenIdConnectConstants.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/AspNet-OpenIdConnect-Server/Owin.Security.OpenIdConnect.Extensions
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace Owin.Security.OpenIdConnect.Extensions {
+    internal static class OpenIdConnectConstants {
+        public static class GrantTypes {
+            public const string AuthorizationCode = "authorization_code";
+            public const string ClientCredentials = "client_credentials";
+            public const string Implicit = "implicit";
+            public const string RefreshToken = "refresh_token";
+            public const string Password = "password";
+        }
+
+        public static class ResponseModes {
+            public const string FormPost = "form_post";
+            public const string Fragment = "fragment";
+            public const string Query = "query";
+        }
+
+        public static class ResponseTypes {
+            public const string Code = "code";
+            public const string IdToken = "id_token";
+            public const string Token = "token";
+        }
+    }
+}
