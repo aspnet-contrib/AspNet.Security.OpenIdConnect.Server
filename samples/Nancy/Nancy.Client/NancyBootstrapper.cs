58522e6c5d1971bf305050b6aa7d3aee423105c6
Pinpoint <kevinchalet@gmail.com>
2015-04-03 21:22:30 +0200

    Update Nancy.Client.csproj to remove the non-cross-platform PostBuildEvent

---
+++ samples/Nancy/Nancy.Client/NancyBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.ViewEngines.Razor;
+
+namespace Nancy.Client {
+    public class NancyBootstrapper : DefaultNancyBootstrapper {
+        protected override IEnumerable<Type> ViewEngines {
+            get {
+                yield return typeof(RazorViewEngine);
+            }
+        }
+    }
+}
