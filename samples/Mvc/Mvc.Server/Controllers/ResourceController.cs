df81d7edd368ed0bcaeddea31d3225008ed98b0e
Pinpoint <kevinchalet@gmail.com>
2015-04-04 15:51:31 +0200

    Update the Mvc.Server sample to set ClaimsIdentity.Actor with the application identity

--- samples/Mvc/Mvc.Server/Controllers/ResourceController.cs
+++ samples/Mvc/Mvc.Server/Controllers/ResourceController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Globalization;
 using System.Net;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace Mvc.Server.Controllers {
     public class ResourceController : ApiController {
         [Authorize, HttpGet, Route("message")]
         public IHttpActionResult GetMessage() {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null) {
+                return InternalServerError();
+            }
+
+            // Note: identity is the ClaimsIdentity representing the resource owner
+            // and identity.Actor is the identity corresponding to the client
+            // application the access token has been issued to (delegation).
             return Content(HttpStatusCode.OK, string.Format(
                 CultureInfo.InvariantCulture,
-                "Hello {0}!",
-                User.Identity.Name));
+                "{0} has been fully authenticated via {1}",
+                identity.Name, identity.Actor.Name));
         }
     }
 }
