9a06e6a99ae767bf778dfc2f64b8871f518373f6
Kévin Chalet <kevinchalet@gmail.com>
2015-04-18 20:59:57 +0200

    Update the repository URL

--- src/AspNet.Security.OpenIdConnect.Server/Notifications/CryptographyEndpointNotification.cs
+++ src/AspNet.Security.OpenIdConnect.Server/Notifications/CryptographyEndpointNotification.cs
@@ -1,6 +1,6 @@
 /*
  * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
- * See https://github.com/AspNet-OpenIdConnect-Server/Owin.Security.OpenIdConnect.Server
+ * See https://github.com/aspnet-contrib/AspNet.Security.OpenIdConnect.Server
  * for more information concerning the license and the contributors participating to this project.
  */
 
