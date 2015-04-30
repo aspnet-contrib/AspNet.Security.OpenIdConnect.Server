73b8d554138e42e2c7c9f29bda6eaf33662dca1e
Kévin Chalet <kevinchalet@gmail.com>
2015-04-30 19:33:00 +0200

    Refactor Startup.cs

--- samples/Mvc/Mvc.Server/Startup.cs
+++ samples/Mvc/Mvc.Server/Startup.cs
@@ -16,21 +16,7 @@ using Owin.Security.OpenIdConnect.Server;
 namespace Mvc.Server {
     public class Startup {
         public void Configuration(IAppBuilder app) {
-            X509Certificate2 certificate;
-
-            // Note: in a real world app, you'd probably prefer storing the X.509 certificate
-            // in the user or machine store. To keep this sample easy to use, the certificate
-            // is extracted from the Certificate.pfx file embedded in this assembly.
-            using (var stream = typeof(Startup).Assembly.GetManifestResourceStream("Mvc.Server.Certificate.pfx"))
-            using (var buffer = new MemoryStream()) {
-                stream.CopyTo(buffer);
-                buffer.Flush();
-
-                certificate = new X509Certificate2(
-                    rawData: buffer.GetBuffer(),
-                    password: "Owin.Security.OpenIdConnect.Server");
-            }
-
+            var certificate = GetCertificate();
             var credentials = new X509SigningCredentials(certificate);
 
             app.Map("/api", map => {
@@ -97,6 +83,21 @@ namespace Mvc.Server {
                 // information concerning ApplicationCanDisplayErrors.
                 ApplicationCanDisplayErrors = true
             });
+        }
+
+        private static X509Certificate2 GetCertificate() {
+            // Note: in a real world app, you'd probably prefer storing the X.509 certificate
+            // in the user or machine store. To keep this sample easy to use, the certificate
+            // is extracted from the Certificate.pfx file embedded in this assembly.
+            using (var stream = typeof(Startup).Assembly.GetManifestResourceStream("Mvc.Server.Certificate.pfx"))
+            using (var buffer = new MemoryStream()) {
+                stream.CopyTo(buffer);
+                buffer.Flush();
+
+                return new X509Certificate2(
+                    rawData: buffer.GetBuffer(),
+                    password: "Owin.Security.OpenIdConnect.Server");
+            }
         }
     }
 }
--- samples/Nancy/Nancy.Server/Startup.cs
+++ samples/Nancy/Nancy.Server/Startup.cs
@@ -16,21 +16,7 @@ using Owin.Security.OpenIdConnect.Server;
 namespace Nancy.Server {
     public class Startup {
         public void Configuration(IAppBuilder app) {
-            X509Certificate2 certificate;
-
-            // Note: in a real world app, you'd probably prefer storing the X.509 certificate
-            // in the user or machine store. To keep this sample easy to use, the certificate
-            // is extracted from the Certificate.pfx file embedded in this assembly.
-            using (var stream = typeof(Startup).Assembly.GetManifestResourceStream("Nancy.Server.Certificate.pfx"))
-            using (var buffer = new MemoryStream()) {
-                stream.CopyTo(buffer);
-                buffer.Flush();
-
-                certificate = new X509Certificate2(
-                    rawData: buffer.GetBuffer(),
-                    password: "Owin.Security.OpenIdConnect.Server");
-            }
-
+            var certificate = GetCertificate();
             var credentials = new X509SigningCredentials(certificate);
 
             app.SetDefaultSignInAsAuthenticationType("ServerCookie");
@@ -92,6 +78,21 @@ namespace Nancy.Server {
             });
 
             app.UseNancy(options => options.Bootstrapper = new NancyBootstrapper());
+        }
+
+        private static X509Certificate2 GetCertificate() {
+            // Note: in a real world app, you'd probably prefer storing the X.509 certificate
+            // in the user or machine store. To keep this sample easy to use, the certificate
+            // is extracted from the Certificate.pfx file embedded in this assembly.
+            using (var stream = typeof(Startup).Assembly.GetManifestResourceStream("Nancy.Server.Certificate.pfx"))
+            using (var buffer = new MemoryStream()) {
+                stream.CopyTo(buffer);
+                buffer.Flush();
+
+                return new X509Certificate2(
+                    rawData: buffer.GetBuffer(),
+                    password: "Owin.Security.OpenIdConnect.Server");
+            }
         }
     }
 }
