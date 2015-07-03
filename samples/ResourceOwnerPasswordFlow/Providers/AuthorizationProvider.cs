290a712503c96ee566f85d54cfd1fbd16e3f4a75
Shaun Luttin <shaunluttin@gmail.com>
2015-07-02 19:41:19 -0700

    Run codeformatter.

--- samples/ResourceOwnerPasswordFlow/Providers/AuthorizationProvider.cs
+++ samples/ResourceOwnerPasswordFlow/Providers/AuthorizationProvider.cs
@@ -53,7 +53,7 @@ namespace ResourceOwnerPasswordFlow.Providers
 
                 var principal = new ClaimsPrincipal(identity);
                 notification.Validated(principal);
-            }            
+            }
         }
     }
 }
