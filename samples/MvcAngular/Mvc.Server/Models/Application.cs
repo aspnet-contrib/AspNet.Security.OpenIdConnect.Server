84106840b4b48d702bd4e6d70d029dca46039994
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2015-07-05 16:09:52 +0200

    Remove Mvc.Server in Angular project.

--- samples/MvcAngular/Mvc.Server/Models/Application.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mvc.Server.Models {
-    public class Application {
-        public string ApplicationID { get; set; }
-        public string DisplayName { get; set; }
-        public string RedirectUri { get; set; }
-        public string LogoutRedirectUri { get; set; }
-        public string Secret { get; set; }
-    }
-}
--- samples/MvcAngular/Mvc.Server/Models/ApplicationContext.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Data.Entity;
-
-namespace Mvc.Server.Models {
-    public class ApplicationContext : DbContext {
-        public DbSet<Application> Applications { get; set; }
-    }
-}
