6cbd633e5840df2992622389d017520ac4376e1a
Shaun Luttin <shaunluttin@gmail.com>
2015-09-01 12:15:46 -0700

    Add an new project, with which to learn how to update to beta8.

---
+++ samples/ROPC/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Configuration;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Dnx.Runtime;
+
+namespace ROPC
+{
+    public class Startup
+    {
+        public IConfiguration Config { get; set; }
+
+        public Startup(IHostingEnvironment hostEnv, IApplicationEnvironment appEnv)
+        {
+            // When running in a different environment than the localhost,
+            // be sure to update either config.json or environmental variables
+            // with appropriate OAuth and OpenId configuration values
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath);
+            builder.AddJsonFile("config.json");
+            builder.AddEnvironmentVariables();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
