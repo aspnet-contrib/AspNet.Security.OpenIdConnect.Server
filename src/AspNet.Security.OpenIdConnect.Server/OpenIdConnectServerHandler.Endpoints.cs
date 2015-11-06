532e6cae3dfaca495112c0b3f73c59a9000f10bd
Kévin Chalet <kevinchalet@gmail.com>
2015-11-06 22:28:26 +0100

    Reject authentication requests containing request or request_uri

--- src/AspNet.Security.OpenIdConnect.Server/OpenIdConnectServerHandler.Endpoints.cs
+++ src/AspNet.Security.OpenIdConnect.Server/OpenIdConnectServerHandler.Endpoints.cs
@@ -196,7 +196,29 @@ namespace AspNet.Security.OpenIdConnect.Server {
                 });
             }
 
-            if (string.IsNullOrEmpty(request.ResponseType)) {
+            if (!string.IsNullOrEmpty(request.GetParameter(OpenIdConnectConstants.Parameters.Request))) {
+                Logger.LogVerbose("The authorization request contained the unsupported request parameter.");
+
+                return await SendErrorRedirectAsync(request, new OpenIdConnectMessage {
+                    Error = OpenIdConnectConstants.Errors.RequestNotSupported,
+                    ErrorDescription = "The request parameter is not supported.",
+                    RedirectUri = request.RedirectUri,
+                    State = request.State
+                });
+            }
+
+            else if (!string.IsNullOrEmpty(request.RequestUri)) {
+                Logger.LogVerbose("The authorization request contained the unsupported request_uri parameter.");
+
+                return await SendErrorRedirectAsync(request, new OpenIdConnectMessage {
+                    Error = OpenIdConnectConstants.Errors.RequestUriNotSupported,
+                    ErrorDescription = "The request_uri parameter is not supported.",
+                    RedirectUri = request.RedirectUri,
+                    State = request.State
+                });
+            }
+
+            else if (string.IsNullOrEmpty(request.ResponseType)) {
                 Logger.LogVerbose("Authorization request missing required response_type parameter");
 
                 return await SendErrorRedirectAsync(request, new OpenIdConnectMessage {
