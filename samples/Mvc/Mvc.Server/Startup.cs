4394a04958e1c5aa2d72fb673e7a6671da6217c5
Kévin Chalet <kevinchalet@gmail.com>
2016-01-09 01:50:39 +0100

    React to API changes in aspnet-contrib/AspNet.Security.OAuth.Extensions
    https://github.com/aspnet-contrib/AspNet.Security.OAuth.Extensions/commit/61115405a583acdae1654d3ec3cc70ccb5fb3ce3

--- samples/Mvc/Mvc.Server/Startup.cs
+++ samples/Mvc/Mvc.Server/Startup.cs
@@ -62,7 +62,7 @@ namespace Mvc.Server {
                 //     options.AutomaticAuthenticate = true;
                 //     options.AutomaticChallenge = true;
                 //     options.Authority = "http://localhost:54540/";
-                //     options.Audience = "resource_server";
+                //     options.Audiences.Add("resource_server");
                 //     options.ClientId = "resource_server";
                 //     options.ClientSecret = "875sqd4s5d748z78z7ds1ff8zz8814ff88ed8ea4z4zzd";
                 // });
