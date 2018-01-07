671bb4766a92be542f34e841678f0bcbc59207d1
Kévin Chalet <kevinchalet@gmail.com>
2018-01-07 18:16:49 +0100

    Update OpenIdConnectMessage/OpenIdConnectParameter to support multi-value parameters

--- src/AspNet.Security.OpenIdConnect.Server/OpenIdConnectServerHandler.Authentication.cs
+++ src/AspNet.Security.OpenIdConnect.Server/OpenIdConnectServerHandler.Authentication.cs
@@ -439,7 +439,7 @@ namespace AspNet.Security.OpenIdConnect.Server
                 notification.ResponseMode =
                     request.IsFormPostResponseMode() ? OpenIdConnectConstants.ResponseModes.FormPost :
                     request.IsFragmentResponseMode() ? OpenIdConnectConstants.ResponseModes.Fragment :
-                    request.IsQueryResponseMode() ? OpenIdConnectConstants.ResponseModes.Query : null;
+                    request.IsQueryResponseMode()    ? OpenIdConnectConstants.ResponseModes.Query    : null;
             }
 
             await Provider.ApplyAuthorizationResponse(notification);
